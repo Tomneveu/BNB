e2dc813bc56130f4d55f82665ca5d93027710b7a
Thomas <tneveu91@gmail.com>
2020-09-09 09:24:32 +0200

    pages admin

--- src/Controller/AdminMaisonsController.php
+++ src/Controller/AdminMaisonsController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\MaisonsRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminMaisonsController extends AbstractController
 {
     /**
      * @Route("/admin/maisons", name="admin_maisons")
      */
-    public function index()
+    public function index(MaisonsRepository $maisonsRepository)
     {
+        $maisons = $maisonsRepository->findAll();
         return $this->render('admin/adminMaisons.html.twig', [
-            'controller_name' => 'AdminMaisonsController',
+            'maisons' => $maisons,
         ]);
     }
 }
